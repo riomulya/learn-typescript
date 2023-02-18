01f67a8d6cf329586e28b5e662652bcf29c05783
RioMulya <riomulya75@gmail.com>
2023-02-18 20:44:54 +0700

    starter

---
+++ using-ts.js
@@ -0,0 +1,6 @@
+var a = 2;
+var b = 10;
+function power(inp1, inp2) {
+    return Math.pow(inp1, inp2);
+}
+console.log(power(a, b));
---
+++ using-ts.ts
@@ -0,0 +1,9 @@
+const a = 2;
+const b = 10;
+
+function power(inp1:number,inp2:number){
+    return inp1 **inp2;
+}
+
+
+console.log(power(a,b+1));
