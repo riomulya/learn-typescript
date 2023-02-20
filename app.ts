eecf0c7e33882809da043012dfafc3bca3fd5dc9
RioMulya <riomulya75@gmail.com>
2023-02-20 07:18:18 +0700

    update config

---
+++ app.ts
@@ -0,0 +1,9 @@
+const person:{
+    name:string;
+    age:number
+} = {
+    name:"Rio",
+    age:20
+}
+
+console.log(person.age)
--- using-ts.ts
+++ using-ts.ts
@@ -2,8 +2,11 @@ const a = 2;
 const b = 10;
 
 function power(inp1:number,inp2:number){
+    console.log(typeof num1)
     return inp1 **inp2;
 }
-
+const num1 = '2';
 
 console.log(power(a,b+1));
+console.log(power(a+1,2.4));
+console.log(power(a+1,2));
