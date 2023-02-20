a9bee456f1e7e91e44992e3367228c908bebd11b
RioMulya <riomulya75@gmail.com>
2023-02-20 08:09:40 +0700

    update

--- app.ts
+++ app.ts
@@ -1,9 +1,19 @@
 const person:{
     name:string;
-    age:number
-} = {
+    age:number;
+    hobbies:string[],
+    role:[string,string]
+}={
     name:"Rio",
-    age:20
+    age:20,
+    hobbies:["swimming","programming"],
+    role:["mahasiswa","dosen"]
 }
 
 console.log(person.age)
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.charAt(0).toUpperCase()+hobby.slice(1))
+}
+
+console.log(person.role[1])
